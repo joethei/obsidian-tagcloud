86c94048f279b76364af98abef3168c142c6894a
Johannes Theiner <kontakt@joethei.xyz>
2022-01-27 20:21:48 +0100

    first basic version

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, getAllTags, MarkdownView, Modal, Notice, parseYaml, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import WordCloud from "wordcloud";
+import cloud from "d3-cloud";
 
 // Remember to rename these classes and interfaces!
 
@@ -14,68 +16,53 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+		console.log("enabling Tag cloud plugin");
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		console.log(WordCloud.isSupported);
+		console.log(WordCloud.miniumFontSize);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerMarkdownCodeBlockProcessor('tagcloud', (source, el, ctx) => {
+			const yaml = parseYaml(source);
+			const max_width = getComputedStyle(
+				document.querySelector(
+					'.markdown-preview-view.is-readable-line-width .markdown-preview-sizer'
+				)
+			).getPropertyValue('max-width');
+			const width = Number(max_width.replace('px', ''));
+			const canvas = el.createEl('canvas', "tagcloud");
+			canvas.width = width;
+			canvas.height = width / 2;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			const tags: string[] = [];
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			//@ts-ignore
+			this.app.metadataCache.getCachedFiles().forEach(filename => {
+				const tmp = getAllTags(this.app.metadataCache.getCache(filename));
+				if (tmp && tmp.length > 0)
+					tags.push(...tmp);
+			});
+
+			const map = tags.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
+
+			const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+			const background = el.style.getPropertyValue("--background-primary");
+
+			//@ts-ignore
+			const searchPlugin = this.app.internalPlugins.getPluginById("global-search");
+			const search = searchPlugin && searchPlugin.instance;
+
+			WordCloud(canvas, {
+				list: Array.from(map.entries()),
+				backgroundColor: background,
+				shape: yaml.shape ? yaml.shape : 'circle',
+				click: item => {
+					search.openGlobalSearch("tag: " + item[0]);
 				}
-			}
+			});
+
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -88,22 +75,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
