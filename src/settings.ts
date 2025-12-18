893c424734b3ee0720668b3aef9c0c078a90498b
Johannes Theiner <kontakt@joethei.xyz>
2025-12-18 19:44:37 +0100

    Update settings tab design to follow Obsidian

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 import TagCloudPlugin from "./main";
-import {PluginSettingTab, Setting} from "obsidian";
+import {PluginSettingTab, Setting, SettingGroup} from "obsidian";
 import WordCloud from "wordcloud";
 
 export interface WordsCache {
@@ -32,6 +32,7 @@ export const DEFAULT_SETTINGS: TagCloudPluginSettings = {
 
 export class TagCloudPluginSettingsTab extends PluginSettingTab{
 	plugin: TagCloudPlugin;
+	icon = 'cloudy';
 
 	constructor(plugin: TagCloudPlugin) {
 		super(plugin.app, plugin);
@@ -46,45 +47,50 @@ export class TagCloudPluginSettingsTab extends PluginSettingTab{
 			containerEl.createEl("p", {cls: "cloud-error", text: "Your device is not supported"});
 		}
 
-		containerEl.createEl('h1', {text: 'Tag & Word Cloud Settings'});
-
-		new Setting(containerEl)
-			.setName('Additional Stopwords')
-			.setDesc("Don't show any of these words in the word cloud(one per line)")
-			.addTextArea(text => {
-					text
-						.setValue(this.plugin.settings.stopwords)
-						.onChange(async (value) => {
-							this.plugin.settings.stopwords = value;
-							await this.plugin.saveSettings();
-						})
-					text.inputEl.setAttr("rows", 8);
-				}
-			);
-
-		new Setting(containerEl)
-			.setDesc("")
-			.addButton(button => {
-				button
-					.setButtonText("Recalculate Word Distribution")
-					.onClick(async () => {
-						await this.plugin.calculateWordDistribution(true);
-					})
+		new SettingGroup(containerEl)
+			.setHeading('Wordcloud')
+			.addSetting(setting => {
+				setting
+					.setName('Additional stopwords')
+					.setDesc("Don't show any of these words in the word cloud(one per line)")
+					.addTextArea(text => {
+							text
+								.setValue(this.plugin.settings.stopwords)
+								.onChange(async (value) => {
+									this.plugin.settings.stopwords = value;
+									await this.plugin.saveSettings();
+								})
+							text.inputEl.setAttr("rows", 8);
+						}
+					);
+			})
+			.addSetting(setting => {
+				setting
+				.setDesc("")
+						.addButton(button => {
+							button
+								.setButtonText("Recalculate word distribution")
+								.onClick(async () => {
+									await this.plugin.calculateWordDistribution(true);
+								})
+						});
 			});
 
-		new Setting(containerEl).setName('Tagcloud').setHeading();
-
-		new Setting(containerEl)
-			.setName('Excluded tags')
-			.setDesc('The following tags wil be excluded from all tag clouds (one per line, without the #)')
-			.addTextArea(text => {
-				//new TagsSuggest(this.app, text.inputEl);
-				text.setValue(this.plugin.settings.tags.exclude.join(', '));
-				text.onChange(async value => {
-					this.plugin.settings.tags.exclude = value.split(', ');
-					await this.plugin.saveSettings();
-				});
-				text.inputEl.setAttr('rows', 10);
+		new SettingGroup(containerEl)
+			.setHeading('Tagcloud')
+			.addSetting(setting => {
+				setting
+					.setName('Excluded tags')
+					.setDesc('The following tags wil be excluded from all tag clouds (one per line, without the #)')
+					.addTextArea(text => {
+						//new TagsSuggest(this.app, text.inputEl);
+						text.setValue(this.plugin.settings.tags.exclude.join(', '));
+						text.onChange(async value => {
+							this.plugin.settings.tags.exclude = value.split(', ');
+							await this.plugin.saveSettings();
+						});
+						text.inputEl.setAttr('rows', 10);
+					});
 			});
 	}
 }
